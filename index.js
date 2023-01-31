807bff2a90d999c543faf2b2d75b57c08cd47b7e
bojanterzic529 <bojanterzic529@gmail.com>
2023-02-01 09:40:14 +1000

    update

--- index.js
+++ index.js
@@ -11,33 +11,35 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.post('/sign', async function (req, res) {
-	const postData = req.body;
-	const options = {
-		hostname: 'botsnightmare.com',
-		path: '/sign',
-		method: 'POST',
-		headers: {
-			'Content-Length': postData.length,
-			'Content-Type': 'application/json'
-		},
+	res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
+	res.send(req.body);
+	// const postData = req.body;
+	// const options = {
+	// 	hostname: 'botsnightmare.com',
+	// 	path: '/sign',
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Length': postData.length,
+	// 		'Content-Type': 'application/json'
+	// 	},
 
-	};
+	// };
 
-	callback = function (response) {
-		var str = '';
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
+	// callback = function (response) {
+	// 	var str = '';
+	// 	response.on('data', function (chunk) {
+	// 		str += chunk;
+	// 	});
 
-		response.on('end', function () {
-			res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
-			res.send(JSON.parse(str))
-		});
-	}
+	// 	response.on('end', function () {
+	// 		res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
+	// 		res.send(JSON.parse(str))
+	// 	});
+	// }
 
-	const reqtoServer = https.request(options, callback);
-	reqtoServer.write(postData);
-	reqtoServer.end();
+	// const reqtoServer = https.request(options, callback);
+	// reqtoServer.write(postData);
+	// reqtoServer.end();
 
 })
 
