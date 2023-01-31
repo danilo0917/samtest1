474334f2b1eb0143ca4b22eedafc9528711a73e0
bojanterzic529 <bojanterzic529@gmail.com>
2023-02-01 09:12:15 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const port = 443;
+
+const app = express();
+const https = require('https')
+
+app.listen(443, () => {
+	console.log(`App start on port ${port}`);
+});
+
+app.get('/sign', async function (req, res) {
+	const postData = req.body;
+	const options = {
+		hostname: 'botsnightmare.com',
+		path: '/sign',
+		method: 'POST',
+		headers: {
+			'Content-Length': postData.length,
+			'Content-Type': 'application/json'
+		},
+
+	};
+
+	callback = function (response) {
+		var str = '';
+		response.on('data', function (chunk) {
+			str += chunk;
+		});
+
+		response.on('end', function () {
+			res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
+			res.send(JSON.parse(str))
+		});
+	}
+
+	const reqtoServer = https.request(options, callback);
+	reqtoServer.write(postData);
+	reqtoServer.end();
+
+})
