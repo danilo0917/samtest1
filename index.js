9d3afac99cc23819930b18577e40bfa238db26c7
bojanterzic529 <bojanterzic529@gmail.com>
2023-02-01 09:35:08 +1000

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require("express");
+require('dotenv').config()
 const port = 5000;
 
 const app = express();
 const https = require('https')
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log(`App start on port ${port}`);
 });
 
