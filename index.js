2337838035b060ba5707adc3771d58ff786e5cb2
bojanterzic529 <bojanterzic529@gmail.com>
2023-02-01 10:09:33 +1000

    update router

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 require('dotenv').config()
 const port = 3000;
 
 const app = express();
 const https = require('https')
 
+app.use("/",router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`App start on port ${process.env.PORT || 3000}`);
 });
 
-app.post('/sign', async function (req, res) {
-	console.log('debug here =======>', req.body)
+router.post('/sign', async function (req, res) {
+	console.log('debug here =======>')
 	res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
 	res.send("yes");
 	// const postData = req.body;
@@ -44,7 +50,7 @@ app.post('/sign', async function (req, res) {
 
 })
 
-app.get('/', function(req, res) {
+router.get('/', function(req, res) {
 	res.set({ 'Content-Type': 'text/json', 'access-control-allow-origin': '*' })
-	res.send('success');
+	res.send('router');
 })
